8b8eb9435e70d6befb868965d7e4b381c99f26dd
James Anthony Lowenthal <loweja01@gmail.com>
2015-10-27 09:02:32 -0400

    Add description to project.clj

--- rick-and-morty/project.clj
+++ rick-and-morty/project.clj
@@ -1,6 +1,6 @@
 (defproject rick-and-morty "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Clojure For The Brave Ch.4 Exercise"
+  :url "https://github.com/JamesAnthonyLow/my-clojure-for-the-brave/tree/master/rick-and-morty"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]]
