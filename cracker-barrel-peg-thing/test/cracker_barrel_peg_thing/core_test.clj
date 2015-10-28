c475f8533e4d24c99513f6dcc56364332b106d1f
James Anthony Lowenthal <loweja01@gmail.com>
2015-10-28 09:24:48 -0400

    add test for tri and triangular?

--- cracker-barrel-peg-thing/test/cracker_barrel_peg_thing/core_test.clj
+++ cracker-barrel-peg-thing/test/cracker_barrel_peg_thing/core_test.clj
@@ -1,7 +1,17 @@
 (ns cracker-barrel-peg-thing.core-test
   (:require [clojure.test :refer :all]
-            [cracker-barrel-peg-thing.core :refer :all]))
+            [cracker-barrel-peg-thing.core :as peg :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest triangular-test
+  (testing "tri"
+    (is
+      (= (take 4 tri)
+         '(1 3 6 10)) "Should return n triangular numbers"))
+  (testing "triangular?"
+    (testing "with triangular number"
+      (is
+        (= (triangular? 3) true))))
+  (testing "with non-triangular number"
+    (is 
+      (= (triangular? 4) false))))
+
