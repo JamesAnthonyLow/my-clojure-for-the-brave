b5ec6a7e2e11f6ed03a9b2434c23a81bca1e6745
James Anthony Lowenthal <loweja01@gmail.com>
2015-10-28 09:24:33 -0400

    add tri and triangular?

--- cracker-barrel-peg-thing/src/cracker_barrel_peg_thing/core.clj
+++ cracker-barrel-peg-thing/src/cracker_barrel_peg_thing/core.clj
@@ -1,5 +1,21 @@
 (ns cracker-barrel-peg-thing.core
+  (:require [clojure.set :as set])
   (:gen-class))
+
+(declare successful-move prompt-move game-over query-rows)
+
+(defn tri*
+  "Generates lazy sequence of triangular numbers"
+  ([] (tri* 0 1)) 
+  ([sum n] (let [new-sum (+ sum n)]
+             (cons new-sum (lazy-seq (tri* new-sum (inc n)))))))
+
+(def tri (tri*))
+
+(defn triangular?
+  "Test if a number is triangular"
+  [n]
+  (= n (last (take-while #(>= n %) tri))))
 
 (defn -main
   "I don't do a whole lot ... yet."
